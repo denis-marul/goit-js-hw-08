1a97e2a210eb0e1f88ca971990f3f9ca408e12cd
denis-marul <marul2na3@gmail.com>
2023-04-12 11:57:58 +0300

    HWeight
    HWeight

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import SimpleLightbox from 'simplelightbox';
+
+import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
+
+const galleryUl = document.querySelector('.gallery');
+
+const galleryElements = galleryItems
+  .map(element => {
+    const { original, preview, description } = element;
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join('');
+
+galleryUl.insertAdjacentHTML('afterbegin', galleryElements);
+
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+  });
+}
+
+galleryUl.addEventListener('click', openModal);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME = 'videoplayer-current-time';
+
+function onPlay(event) {
+  localStorage.setItem(TIME, event.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function setCurrentTime() {
+  if (!localStorage.getItem(TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(TIME));
+}
+setCurrentTime();
